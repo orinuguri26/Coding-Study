bb3aea25ef26d7066420b0fe362d33dae64f5445
ori_nuguri <wpdnrdl1026@gmail.com>
2026-01-25 22:54:03 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1552 ms, Memory: 339440 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -4,19 +4,20 @@ import java.lang.*;
 
 public class Main
 {
-    //10989번 수 정렬하기 3
     public static void main(String[] args) throws IOException
     {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int[] num = new int[N];
+        int[] num = new int[10_001];
 
-        for(int i = 0; i < N; i++) num[i] = Integer.parseInt(br.readLine());
-        Arrays.sort(num);
-        
+        for(int i = 0; i < N; i++) num[Integer.parseInt(br.readLine())]++;
+
         StringBuilder sb = new StringBuilder();
-        for(int a : num) sb.append(a).append('\n');
-        
+        for(int i = 1; i < num.length; i++)
+        {
+            if(num[i] == 0) continue;
+            while(num[i] -- > 0) sb.append(i).append('\n');
+        }
         System.out.print(sb);
     }
 }
