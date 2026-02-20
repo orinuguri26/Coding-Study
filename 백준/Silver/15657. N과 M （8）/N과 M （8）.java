c66df41d4c177c3a226edd41de34c264a5d7944a
ori_nuguri <wpdnrdl1026@gmail.com>
2026-02-20 22:01:37 +0900

    [Silver III] Title: N과 M (8), Time: 136 ms, Memory: 18076 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+public class Main {
+    //15657번 N과 M (8)
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+    static int N, M;
+    static int[] num;
+    static int[] result;
+    static void NtoM(int idx, int rst) {
+        if(rst == M) {
+            for(int i : result) {
+                sb.append(i).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+        for(int i = idx; i < N; i++) {
+            result[rst] = num[i];
+            NtoM(i, rst + 1);
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        num = new int[N];
+        result = new int[M];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) num[i] = Integer.parseInt(st.nextToken());
+        Arrays.sort(num);
+
+        NtoM(0,0);
+        System.out.print(sb);
+    }
+}
