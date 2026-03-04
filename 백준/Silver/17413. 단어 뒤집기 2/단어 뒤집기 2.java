0c7ba90187583843867b82b704b076dfaf41fd24
ori_nuguri <wpdnrdl1026@gmail.com>
2026-03-04 20:46:07 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 228 ms, Memory: 23256 KB -BaekjoonHub

---
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+public class Main {
+    //17413번 단어 뒤집기2
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        String str = br.readLine();
+        boolean tag = false;
+        Stack<Character> s = new Stack<>();
+        for(int i = 0; i < str.length(); i++) {
+            if(str.charAt(i) == '<') {
+                if(!s.isEmpty()) while(!s.isEmpty()) sb.append(s.pop());
+                tag = true;
+            }
+            if(str.charAt(i) == '>') {
+                sb.append(str.charAt(i));
+                tag = false;
+                continue;
+            }
+            if(!tag) {
+                if(str.charAt(i) == ' ') {
+                    while(!s.isEmpty()) sb.append(s.pop());
+                    sb.append(' ');
+                }
+                else s.push(str.charAt(i));
+            }
+            if(tag) sb.append(str.charAt(i));
+        }
+        while(!s.isEmpty()) sb.append(s.pop());
+        System.out.print(sb);
+    }
+}
