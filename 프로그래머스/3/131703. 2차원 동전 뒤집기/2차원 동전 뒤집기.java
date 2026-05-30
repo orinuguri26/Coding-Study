93c619492d9d008f1e6f87a795ff0a2748049852
ori_nuguri <wpdnrdl1026@gmail.com>
2026-05-30 22:23:07 +0900

    [level 3] Title: 2차원 동전 뒤집기, Time: 0.05 ms, Memory: 62.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/131703. 2차원 동전 뒤집기/2차원 동전 뒤집기.java
@@ -0,0 +1,51 @@
+import java.util.*;
+class Solution {
+    //0 -> 앞면, 검은색 / 1 -> 뒷면, 흰색
+    int[][] target, beginning;
+    boolean[] rflip;
+    int n, m;
+    int cnt = 0;
+    public boolean cflip() {
+        boolean go = true;
+        for(int i = 0; i < m; i++) {
+            boolean valid = true;
+            go = true;
+            for(int j = 0; j < n; j++) {
+                int cur = rflip[j] ? (beginning[j][i] + 1) % 2 : beginning[j][i];
+                if(j == 0 && cur != target[0][i]) {
+                    valid = false;
+                    cnt++;
+                }
+                if(valid) {
+                    if(cur != target[j][i]) go = false;
+                }
+                else {
+                    if(cur == target[j][i]) go = false;
+                }
+            }
+            if(!go) break;
+        }
+        return go;
+    }
+    public int solution(int[][] beginning, int[][] target) {
+        this.beginning = beginning;
+        this.target = target;
+        n = beginning.length;
+        m = beginning[0].length;
+        int answer = Integer.MAX_VALUE;
+        
+        for(int t = 0; t < 2; t++) {
+            cnt = 0;
+            rflip = new boolean[n];
+            for(int i = 0; i < n; i++) { 
+                if((beginning[i][0] + t) % 2 != target[i][0]) {
+                    rflip[i] = true;
+                    cnt++;
+                }
+            }
+            if(cflip()) answer = Math.min(answer, cnt);
+        }
+
+        return answer == Integer.MAX_VALUE ? -1 : answer;
+    }
+}
